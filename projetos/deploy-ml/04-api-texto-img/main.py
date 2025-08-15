3ac4f4b28e2f041efa7efdfc0c1db2027b43359a
Priscila Dalepiane <prisciladalepiane@gmail.com>
2025-08-15 14:02:22 -0300

    Finalizando a API com execução WSGI

--- projetos/deploy-ml/04-api-texto-img/main.py
+++ projetos/deploy-ml/04-api-texto-img/main.py
@@ -25,3 +25,7 @@ async def api(text: str = Form(...), image: UploadFile = File(...)):
     resultado = pipeline_modelo(text, image)
 
     return {"Resposta:", resultado}
+
+# Inicia o servidor WSGI Uvicorn com API
+if __name__ == "__main__":
+    uvicorn.run(app, host = "0.0.0.0", port = 3000)
