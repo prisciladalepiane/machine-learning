f0adca3c40cce996d788273cf24ccd8ff4116e4f
Priscila Dalepiane <prisciladalepiane@gmail.com>
2025-08-15 13:02:06 -0300

    Transformado pipeline em função para receber o texto e a imagem e retornar a resposta

--- projetos/deploy-ml/04-api-texto-img/modelo.py
+++ projetos/deploy-ml/04-api-texto-img/modelo.py
@@ -5,18 +5,23 @@ from PIL import Image
 processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
+def pipeline_modelo(text:str, image:Image):
+    # prepare inputs
+    encoding = processor(image, text, return_tensors="pt")
+
+    outputs = model(**encoding)
+    logits = outputs.logits
+
+    idx = logits.argmax(-1).item()
+    return model.config.id2label[idx]
+
 
 # prepare image + question
-url = "projetos/deploy-ml/04-api-texto-imagem-llm/img/imagem3.jpg"
+url = "projetos/deploy-ml/04-api-texto-img/img/imagem3.jpg"
 image = Image.open(url)
+
 text = "what kind of dog is it?"
 
+answer = pipeline_modelo(text, image)
 
-# prepare inputs
-encoding = processor(image, text, return_tensors="pt")
-
-# forward pass
-outputs = model(**encoding)
-logits = outputs.logits
-idx = logits.argmax(-1).item()
-print("Predicted answer:", model.config.id2label[idx]) 
+print("Answer:", answer)
